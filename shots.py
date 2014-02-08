efbb2419ef44f945a52a6026f4f3972ae4182426
Jason Berlinsky <jason@barefootcoders.com>
2014-02-08 17:37:19 -0500

    Beef up video conversion

--- shots.py
+++ shots.py
@@ -76,7 +76,8 @@ def send(filetype):
 		snapformat = Snapchat.MEDIA_IMAGE
 	if (filetype == "video"):
 		snapformat = Snapchat.MEDIA_VIDEO
-                call(['ffmpeg', '-i', filename, '-vf', '"transpose=0"', 'uploaded_transposed_file.mp4'])
+                call(['rm -rf uploaded_transposed_file.mp4'])
+                call(['ffmpeg -i ' + filename + ' -vf "transpose=0" uploaded_transposed_file.mp4'])
                 filename = 'uploaded_transposed_file.mp4'
                 
 
