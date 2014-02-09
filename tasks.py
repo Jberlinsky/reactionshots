e57eaba81ba5f4c13af7a9dd2505e91736e96641
Jason Berlinsky <jason@barefootcoders.com>
2014-02-09 06:14:07 -0500

    Video transposition logging

--- tasks.py
+++ tasks.py
@@ -10,24 +10,20 @@ bg = Celery('tasks', broker='amqp://guest@localhost//')
 logger = get_task_logger(__name__)
 @bg.task
 def upload_file(username, password, filename, filetype, recipients):
-        logger.debug("GOT HERE")
-        print "ALKSJDLKASJLKD"
         s = Snapchat()
         s.login(username, password)
-        logger.debug('Parsing')
         #upload file to snapchat
         if (filetype == "image"):
                 snapformat = Snapchat.MEDIA_IMAGE
         if (filetype == "video"):
                 snapformat = Snapchat.MEDIA_VIDEO
+                print "Setting up new filename"
                 new_filename = replace(filename, '.mp4', '_transposed.mp4')
+                print "New filename: " + filename
                 os.system('rm -rf ' + new_filename)
+                print "Empty location"
                 os.system('ffmpeg -i ' + filename + ' -vf "transpose=0" ' + new_filename)
+                print "TRANSPOSED!"
                 filename = new_filename
-        logger.debug("Sending...")
-
         media_id = s.upload(snapformat, filename)
-
-        logger.debug("Notifying")
-
         return s.send(media_id, split(recipients, ','), 5)
